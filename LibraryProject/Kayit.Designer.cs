d48febbb2b5d344c6a2cb45e7dc46723df8c6c33
Betül Bedel <bedel69@hotmail.com>
2022-12-06 19:44:22 +0300

    First Commit

---
+++ LibraryProject/Kayit.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace LibraryProject
+{
+    partial class Kayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.isimtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.soyisimtextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.kullaniciaditextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mailtextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sifretextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sifrekontroltextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sifreinfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // isimtextBox
+            // 
+            this.isimtextBox.Location = new System.Drawing.Point(189, 56);
+            this.isimtextBox.Name = "isimtextBox";
+            this.isimtextBox.Size = new System.Drawing.Size(150, 31);
+            this.isimtextBox.TabIndex = 0;
+            this.isimtextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.isimtextBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.isimtextBox_PreviewKeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "İsim";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Soyisim";
+            // 
+            // soyisimtextBox
+            // 
+            this.soyisimtextBox.Location = new System.Drawing.Point(189, 123);
+            this.soyisimtextBox.Name = "soyisimtextBox";
+            this.soyisimtextBox.Size = new System.Drawing.Size(150, 31);
+            this.soyisimtextBox.TabIndex = 3;
+            this.soyisimtextBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.soyisimtextBox_PreviewKeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kullanıcı Adı";
+            // 
+            // kullaniciaditextBox
+            // 
+            this.kullaniciaditextBox.Location = new System.Drawing.Point(189, 191);
+            this.kullaniciaditextBox.Name = "kullaniciaditextBox";
+            this.kullaniciaditextBox.Size = new System.Drawing.Size(150, 31);
+            this.kullaniciaditextBox.TabIndex = 5;
+            this.kullaniciaditextBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.kullaniciaditextBox_PreviewKeyDown_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Şifre";
+            // 
+            // mailtextBox
+            // 
+            this.mailtextBox.Location = new System.Drawing.Point(189, 251);
+            this.mailtextBox.Name = "mailtextBox";
+            this.mailtextBox.Size = new System.Drawing.Size(150, 31);
+            this.mailtextBox.TabIndex = 7;
+            this.mailtextBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.mailtextBox_PreviewKeyDown_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 380);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Şifre(Tekrar)";
+            // 
+            // sifretextBox
+            // 
+            this.sifretextBox.Location = new System.Drawing.Point(189, 310);
+            this.sifretextBox.Name = "sifretextBox";
+            this.sifretextBox.Size = new System.Drawing.Size(150, 31);
+            this.sifretextBox.TabIndex = 9;
+            this.sifretextBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.sifretextBox_PreviewKeyDown);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 257);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Mail Adresi";
+            // 
+            // sifrekontroltextBox
+            // 
+            this.sifrekontroltextBox.Location = new System.Drawing.Point(189, 374);
+            this.sifrekontroltextBox.Name = "sifrekontroltextBox";
+            this.sifrekontroltextBox.Size = new System.Drawing.Size(150, 31);
+            this.sifrekontroltextBox.TabIndex = 11;
+            this.sifrekontroltextBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.sifrekontroltextBox_PreviewKeyDown_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Kayıt Ol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sifreinfo
+            // 
+            this.sifreinfo.AutoSize = true;
+            this.sifreinfo.Location = new System.Drawing.Point(417, 313);
+            this.sifreinfo.Name = "sifreinfo";
+            this.sifreinfo.Size = new System.Drawing.Size(0, 25);
+            this.sifreinfo.TabIndex = 14;
+            // 
+            // Kayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 524);
+            this.Controls.Add(this.sifreinfo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.sifrekontroltextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sifretextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mailtextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.kullaniciaditextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.soyisimtextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.isimtextBox);
+            this.Name = "Kayit";
+            this.Text = "Kayit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox isimtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox soyisimtextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox kullaniciaditextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox mailtextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox sifretextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox sifrekontroltextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label sifreinfo;
+    }
+}
---
+++ LibraryProject/Kayit.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryProject
+{
+    public partial class Kayit : Form
+    {
+        public Kayit()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isemptytextboxexist=false;
+            if (isimtextBox.Text == string.Empty)
+            {
+                isimtextBox.BackColor = Color.Red;
+                isemptytextboxexist = true;
+
+            }
+            if (soyisimtextBox.Text == string.Empty)
+            {
+                soyisimtextBox.BackColor = Color.Red;
+                isemptytextboxexist = true;
+            }
+            if (kullaniciaditextBox.Text == string.Empty)
+            {
+                kullaniciaditextBox.BackColor = Color.Red;
+                isemptytextboxexist = true;
+            }
+            if (mailtextBox.Text == string.Empty)
+            {
+                mailtextBox.BackColor = Color.Red;
+                isemptytextboxexist = true;
+            }
+
+            if (sifretextBox.Text == string.Empty)
+            {
+                sifretextBox.BackColor = Color.Red;
+                isemptytextboxexist = true;
+            }
+            if (sifrekontroltextBox.Text == string.Empty)
+            {
+                sifrekontroltextBox.BackColor = Color.Red;
+                isemptytextboxexist = true;
+            }
+
+
+            if (isemptytextboxexist)
+            {
+                return;
+            }
+
+            if (sifretextBox.Text.Length < 6)
+            {
+                sifreinfo.Text = "Şifre uzunluğu 6 karakterden büyük olmalıdır.";
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void isimtextBox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            isimtextBox.BackColor = Color.White;
+        }
+
+        private void sifretextBox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            sifretextBox.BackColor = Color.White;
+            sifreinfo.Text = string.Empty;
+        }
+
+        private void soyisimtextBox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            soyisimtextBox.BackColor = Color.White;
+        }
+        private void kullaniciaditextBox_PreviewKeyDown_1(object sender, PreviewKeyDownEventArgs e)
+        {
+            kullaniciaditextBox.BackColor = Color.White;
+        }
+
+        private void mailtextBox_PreviewKeyDown_1(object sender, PreviewKeyDownEventArgs e)
+        {
+            mailtextBox.BackColor = Color.White;
+        }
+
+        private void sifrekontroltextBox_PreviewKeyDown_1(object sender, PreviewKeyDownEventArgs e)
+        {
+            sifrekontroltextBox.BackColor = Color.White;
+        }
+    }
+}
