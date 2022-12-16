5296b40e700a9ae562f32ad8d8b25cd65558aa75
Betül Bedel <bedel69@hotmail.com>
2022-12-16 21:42:46 +0300

    Menu Design

---
+++ LibraryProject/MenuPanel.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace LibraryProject
+{
+    partial class MenuPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kitapeklepictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.kitapeklebutton = new System.Windows.Forms.Button();
+            this.uyeklebutton = new System.Windows.Forms.Button();
+            this.oduncbutton = new System.Windows.Forms.Button();
+            this.admineklebutton = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.uyeislembutton = new System.Windows.Forms.Button();
+            this.odunctakipbutton = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.cikisbutton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kitapeklepictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kitapeklepictureBox1
+            // 
+            this.kitapeklepictureBox1.Image = global::LibraryProject.Properties.Resources.kitap2;
+            this.kitapeklepictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.kitapeklepictureBox1.Name = "kitapeklepictureBox1";
+            this.kitapeklepictureBox1.Size = new System.Drawing.Size(175, 147);
+            this.kitapeklepictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kitapeklepictureBox1.TabIndex = 0;
+            this.kitapeklepictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LibraryProject.Properties.Resources.uyeekle;
+            this.pictureBox2.Location = new System.Drawing.Point(262, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LibraryProject.Properties.Resources.odunc3;
+            this.pictureBox3.Location = new System.Drawing.Point(539, 27);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::LibraryProject.Properties.Resources.kutuphane;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 326);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::LibraryProject.Properties.Resources.uye4;
+            this.pictureBox5.Location = new System.Drawing.Point(262, 326);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::LibraryProject.Properties.Resources.kitap3;
+            this.pictureBox6.Location = new System.Drawing.Point(539, 326);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::LibraryProject.Properties.Resources.admin;
+            this.pictureBox7.Location = new System.Drawing.Point(816, 27);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::LibraryProject.Properties.Resources.admin2;
+            this.pictureBox8.Location = new System.Drawing.Point(816, 326);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // kitapeklebutton
+            // 
+            this.kitapeklebutton.BackColor = System.Drawing.Color.Chocolate;
+            this.kitapeklebutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.kitapeklebutton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.kitapeklebutton.Location = new System.Drawing.Point(12, 198);
+            this.kitapeklebutton.Name = "kitapeklebutton";
+            this.kitapeklebutton.Size = new System.Drawing.Size(175, 43);
+            this.kitapeklebutton.TabIndex = 8;
+            this.kitapeklebutton.Text = "KİTAP EKLE";
+            this.kitapeklebutton.UseVisualStyleBackColor = false;
+            this.kitapeklebutton.Click += new System.EventHandler(this.kitapeklebutton_Click);
+            // 
+            // uyeklebutton
+            // 
+            this.uyeklebutton.BackColor = System.Drawing.Color.Chocolate;
+            this.uyeklebutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.uyeklebutton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.uyeklebutton.Location = new System.Drawing.Point(262, 198);
+            this.uyeklebutton.Name = "uyeklebutton";
+            this.uyeklebutton.Size = new System.Drawing.Size(175, 43);
+            this.uyeklebutton.TabIndex = 9;
+            this.uyeklebutton.Text = "ÜYE EKLE";
+            this.uyeklebutton.UseVisualStyleBackColor = false;
+            this.uyeklebutton.Click += new System.EventHandler(this.uyeklebutton_Click);
+            // 
+            // oduncbutton
+            // 
+            this.oduncbutton.BackColor = System.Drawing.Color.Chocolate;
+            this.oduncbutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.oduncbutton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.oduncbutton.Location = new System.Drawing.Point(539, 198);
+            this.oduncbutton.Name = "oduncbutton";
+            this.oduncbutton.Size = new System.Drawing.Size(175, 43);
+            this.oduncbutton.TabIndex = 10;
+            this.oduncbutton.Text = "ÖDÜNÇ VER";
+            this.oduncbutton.UseVisualStyleBackColor = false;
+            // 
+            // admineklebutton
+            // 
+            this.admineklebutton.BackColor = System.Drawing.Color.Chocolate;
+            this.admineklebutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.admineklebutton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.admineklebutton.Location = new System.Drawing.Point(816, 198);
+            this.admineklebutton.Name = "admineklebutton";
+            this.admineklebutton.Size = new System.Drawing.Size(175, 43);
+            this.admineklebutton.TabIndex = 11;
+            this.admineklebutton.Text = "ADMİN EKLE";
+            this.admineklebutton.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Chocolate;
+            this.button5.Font = new System.Drawing.Font("Book Antiqua", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(12, 494);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(175, 43);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "KİTAP İŞLEMLERİ";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // uyeislembutton
+            // 
+            this.uyeislembutton.BackColor = System.Drawing.Color.Chocolate;
+            this.uyeislembutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.uyeislembutton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.uyeislembutton.Location = new System.Drawing.Point(262, 494);
+            this.uyeislembutton.Name = "uyeislembutton";
+            this.uyeislembutton.Size = new System.Drawing.Size(175, 43);
+            this.uyeislembutton.TabIndex = 13;
+            this.uyeislembutton.Text = "ÜYE İŞLEMLERİ";
+            this.uyeislembutton.UseVisualStyleBackColor = false;
+            this.uyeislembutton.Click += new System.EventHandler(this.uyeislembutton_Click);
+            // 
+            // odunctakipbutton
+            // 
+            this.odunctakipbutton.BackColor = System.Drawing.Color.Chocolate;
+            this.odunctakipbutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.odunctakipbutton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.odunctakipbutton.Location = new System.Drawing.Point(539, 494);
+            this.odunctakipbutton.Name = "odunctakipbutton";
+            this.odunctakipbutton.Size = new System.Drawing.Size(175, 43);
+            this.odunctakipbutton.TabIndex = 14;
+            this.odunctakipbutton.Text = "ÖDÜNÇ TAKİP";
+            this.odunctakipbutton.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Chocolate;
+            this.button8.Font = new System.Drawing.Font("Book Antiqua", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button8.Location = new System.Drawing.Point(816, 494);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(175, 43);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "ADMİN İŞLEMLERİ";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // cikisbutton
+            // 
+            this.cikisbutton.BackColor = System.Drawing.Color.Chocolate;
+            this.cikisbutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.cikisbutton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.cikisbutton.Location = new System.Drawing.Point(834, 739);
+            this.cikisbutton.Name = "cikisbutton";
+            this.cikisbutton.Size = new System.Drawing.Size(175, 43);
+            this.cikisbutton.TabIndex = 17;
+            this.cikisbutton.Text = "ÇIKIŞ YAP";
+            this.cikisbutton.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LibraryProject.Properties.Resources.logout;
+            this.pictureBox1.Location = new System.Drawing.Point(834, 571);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MenuPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(1051, 802);
+            this.Controls.Add(this.cikisbutton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.odunctakipbutton);
+            this.Controls.Add(this.uyeislembutton);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.admineklebutton);
+            this.Controls.Add(this.oduncbutton);
+            this.Controls.Add(this.uyeklebutton);
+            this.Controls.Add(this.kitapeklebutton);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.kitapeklepictureBox1);
+            this.Name = "MenuPanel";
+            this.Text = "MenuPanel";
+            ((System.ComponentModel.ISupportInitialize)(this.kitapeklepictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox kitapeklepictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button kitapeklebutton;
+        private System.Windows.Forms.Button uyeklebutton;
+        private System.Windows.Forms.Button oduncbutton;
+        private System.Windows.Forms.Button admineklebutton;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button uyeislembutton;
+        private System.Windows.Forms.Button odunctakipbutton;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button cikisbutton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ LibraryProject/MenuPanel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryProject
+{
+    public partial class MenuPanel : Form
+    {
+        public MenuPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void kitapeklebutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AdminPanel adminpanel = new AdminPanel();
+            adminpanel.Show();
+        }
+
+        private void uyeislembutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UyeIslemPanel kitapislem = new UyeIslemPanel();
+            kitapislem.Show();
+        }
+
+        private void uyeklebutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UyeEklePanel uyeklepanel = new UyeEklePanel();
+            uyeklepanel.Show();
+        }
+    }
+}
