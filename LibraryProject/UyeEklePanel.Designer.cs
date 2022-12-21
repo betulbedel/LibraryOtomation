70b5b00fd9157b9ea569e36d7aeb31d98a13c7ef
Betül Bedel <bedel69@hotmail.com>
2022-12-21 21:59:56 +0300

    User acception menu panel added

---
+++ LibraryProject/UyeEklePanel.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace LibraryProject
+{
+    partial class UyeEklePanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.kullaniciaditextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.soyisimtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.isimtextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.kadinradioButton = new System.Windows.Forms.RadioButton();
+            this.erkekradioButton = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mailtextBox = new System.Windows.Forms.TextBox();
+            this.uyeklebutton = new System.Windows.Forms.Button();
+            this.anasayfabutton = new System.Windows.Forms.Button();
+            this.uyeklelabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label3.Location = new System.Drawing.Point(33, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 23);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Kullanıcı Adı:";
+            // 
+            // kullaniciaditextBox
+            // 
+            this.kullaniciaditextBox.Location = new System.Drawing.Point(174, 227);
+            this.kullaniciaditextBox.Name = "kullaniciaditextBox";
+            this.kullaniciaditextBox.Size = new System.Drawing.Size(150, 31);
+            this.kullaniciaditextBox.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Location = new System.Drawing.Point(59, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Soyisim:";
+            // 
+            // soyisimtextBox
+            // 
+            this.soyisimtextBox.Location = new System.Drawing.Point(174, 159);
+            this.soyisimtextBox.Name = "soyisimtextBox";
+            this.soyisimtextBox.Size = new System.Drawing.Size(150, 31);
+            this.soyisimtextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(86, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "İsim:";
+            // 
+            // isimtextBox
+            // 
+            this.isimtextBox.Location = new System.Drawing.Point(174, 92);
+            this.isimtextBox.Name = "isimtextBox";
+            this.isimtextBox.Size = new System.Drawing.Size(150, 31);
+            this.isimtextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label4.Location = new System.Drawing.Point(72, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 23);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Cinsiyet:";
+            // 
+            // kadinradioButton
+            // 
+            this.kadinradioButton.AutoSize = true;
+            this.kadinradioButton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.kadinradioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.kadinradioButton.Location = new System.Drawing.Point(171, 286);
+            this.kadinradioButton.Name = "kadinradioButton";
+            this.kadinradioButton.Size = new System.Drawing.Size(84, 27);
+            this.kadinradioButton.TabIndex = 14;
+            this.kadinradioButton.TabStop = true;
+            this.kadinradioButton.Text = "Kadın";
+            this.kadinradioButton.UseVisualStyleBackColor = true;
+            // 
+            // erkekradioButton
+            // 
+            this.erkekradioButton.AutoSize = true;
+            this.erkekradioButton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.erkekradioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.erkekradioButton.Location = new System.Drawing.Point(261, 286);
+            this.erkekradioButton.Name = "erkekradioButton";
+            this.erkekradioButton.Size = new System.Drawing.Size(81, 27);
+            this.erkekradioButton.TabIndex = 15;
+            this.erkekradioButton.TabStop = true;
+            this.erkekradioButton.Text = "Erkek";
+            this.erkekradioButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label5.Location = new System.Drawing.Point(46, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 23);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Mail Adresi:";
+            // 
+            // mailtextBox
+            // 
+            this.mailtextBox.Location = new System.Drawing.Point(174, 346);
+            this.mailtextBox.Name = "mailtextBox";
+            this.mailtextBox.Size = new System.Drawing.Size(150, 31);
+            this.mailtextBox.TabIndex = 16;
+            // 
+            // uyeklebutton
+            // 
+            this.uyeklebutton.BackColor = System.Drawing.Color.Chocolate;
+            this.uyeklebutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.uyeklebutton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.uyeklebutton.Location = new System.Drawing.Point(171, 541);
+            this.uyeklebutton.Name = "uyeklebutton";
+            this.uyeklebutton.Size = new System.Drawing.Size(138, 52);
+            this.uyeklebutton.TabIndex = 18;
+            this.uyeklebutton.Text = "EKLE";
+            this.uyeklebutton.UseVisualStyleBackColor = false;
+            // 
+            // anasayfabutton
+            // 
+            this.anasayfabutton.BackColor = System.Drawing.Color.Peru;
+            this.anasayfabutton.Font = new System.Drawing.Font("Book Antiqua", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.anasayfabutton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.anasayfabutton.Location = new System.Drawing.Point(476, 541);
+            this.anasayfabutton.Name = "anasayfabutton";
+            this.anasayfabutton.Size = new System.Drawing.Size(168, 52);
+            this.anasayfabutton.TabIndex = 19;
+            this.anasayfabutton.Text = "Anasayfaya Dön";
+            this.anasayfabutton.UseVisualStyleBackColor = false;
+            this.anasayfabutton.Click += new System.EventHandler(this.anasayfabutton_Click);
+            // 
+            // uyeklelabel
+            // 
+            this.uyeklelabel.AutoSize = true;
+            this.uyeklelabel.Font = new System.Drawing.Font("Book Antiqua", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.uyeklelabel.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.uyeklelabel.Location = new System.Drawing.Point(161, 24);
+            this.uyeklelabel.Name = "uyeklelabel";
+            this.uyeklelabel.Size = new System.Drawing.Size(130, 29);
+            this.uyeklelabel.TabIndex = 20;
+            this.uyeklelabel.Text = "ÜYE EKLE";
+            // 
+            // UyeEklePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(808, 673);
+            this.Controls.Add(this.uyeklelabel);
+            this.Controls.Add(this.anasayfabutton);
+            this.Controls.Add(this.uyeklebutton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.mailtextBox);
+            this.Controls.Add(this.erkekradioButton);
+            this.Controls.Add(this.kadinradioButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.kullaniciaditextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.soyisimtextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.isimtextBox);
+            this.Name = "UyeEklePanel";
+            this.Text = "UyeEklePanel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox kullaniciaditextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox soyisimtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox isimtextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton kadinradioButton;
+        private System.Windows.Forms.RadioButton erkekradioButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox mailtextBox;
+        private System.Windows.Forms.Button uyeklebutton;
+        private System.Windows.Forms.Button anasayfabutton;
+        private System.Windows.Forms.Label uyeklelabel;
+    }
+}
---
+++ LibraryProject/UyeEklePanel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryProject
+{
+    public partial class UyeEklePanel : Form
+    {
+        public UyeEklePanel()
+        {
+            InitializeComponent();
+        }
+
+        private void anasayfabutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MenuPanel menupanel = new MenuPanel();
+            menupanel.Show();
+        }
+    }
+}
